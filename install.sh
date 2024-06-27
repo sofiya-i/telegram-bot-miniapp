7fc07dd3bb83d4912ab51da879b61125526bee12
i_Sofiya__ <151798930+sofiya-i@users.noreply.github.com>
2024-06-27 05:52:31 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,20 @@
 
 # به روز رسانی و نصب پیش‌نیازها
 sudo apt update
-sudo apt install python3 python3-pip python3-venv -y
+sudo apt install python3 python3-pip git -y
 
 # کلون کردن مخزن
 git clone https://github.com/sofiya-i/telegram-bot-miniapp.git
 cd telegram-bot-miniapp
 
-# ایجاد محیط مجازی و نصب کتابخانه‌ها
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+# نصب کتابخانه‌ها به صورت global
+pip3 install -r requirements.txt
 
 # یادآوری برای پیکربندی فایل config.py
 echo "Please configure the config.py file with your bot token and other settings."
 
 # اجرای ربات
-echo "To run the bot, use: source venv/bin/activate && python bot.py"
+echo "To run the bot, use: python3 bot.py"
 
 # پایان
 echo "Installation complete. Please configure the bot and run it."
